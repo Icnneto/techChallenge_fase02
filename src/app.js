c25923d041044c9a84ab96eb9e8346e9b2acafd7
Israel Nunes <israelnetonunes@gmail.com>
2025-09-16 10:11:57 -0300

    feat: adicionar rota em app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const postRoutes = require('./routes/postRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/posts', postRoutes);
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.status(200).send('API is running');
